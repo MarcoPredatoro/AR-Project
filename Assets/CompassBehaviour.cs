1d1726c870068e1cb2ba64644b3153aff74d6813
carla-rosario <78482746+carla-rosario@users.noreply.github.com>
2023-03-13 16:46:40 +0000

    Compass follows live marco

--- Assets/CompassBehaviour.cs
+++ Assets/CompassBehaviour.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using System.IO;
+using Photon.Realtime;
 
+//Attach to Marco prefab
 public class CompassBehaviour : MonoBehaviour
 {   
 
     private Texture2D arrow;
-    public Camera arCamera;
-    public GameObject Marco;
-    // PhotonView MarcoView;
+    Camera arCamera;
+    GameObject Marco;
+    PhotonView MarcoView;
     private float angle = 0f;
     
     void Start() {
@@ -20,16 +22,20 @@ public class CompassBehaviour : MonoBehaviour
         byte[] imageBytes = File.ReadAllBytes("Assets/kisspng-computer-icons-automotive-navigation-system-5-arrow-5b28aacfb73147.7266884915293918237504.png");
         // Load the byte array into a texture
         arrow.LoadImage(imageBytes);
+
+        //Find camera
+        arCamera = Camera.main;
     }
 
     void Update() {
-        // Get the PhotonView component of Marco if hasn't been done already
-        // if (!MarcoView) {
-        //     MarcoView = Marco.GetComponent<PhotonView>();
-        // } 
+        
         //Find vector from camera to Marco
-        Vector3 bet = Marco.transform.position - arCamera.transform.position;
+        //MarcoView = GetComponent<PhotonView>();
+        Vector3 bet = transform.position - arCamera.transform.position;
+
         bet.y = 0f; //Ignore vertical component of vector
+        Debug.Log("Marco position: " + transform.position);
+        Debug.Log("Camera position: " + arCamera.transform.position);
 
         //Find forward vector of camera
         Vector3 camForward = arCamera.transform.forward;
@@ -38,6 +44,7 @@ public class CompassBehaviour : MonoBehaviour
         //Update angle between camera forward vector and vector from camera to Marco, about y axis
         angle = Vector3.SignedAngle(camForward, bet, Vector3.up); //Returns angle in degrees between -180 and 180
         Debug.Log("Angle: " + angle);
+        
        
     }
 
@@ -46,6 +53,18 @@ public class CompassBehaviour : MonoBehaviour
         GUIUtility.RotateAroundPivot(angle, new Vector2(Screen.width/2, Screen.height/2));
         GUI.DrawTexture(new Rect(Screen.width/2 - arrow.width/2, Screen.height/2 - arrow.height/2, arrow.width, arrow.height), arrow);
     }
+
+    //Don't need if script is attached to Marco prefab
+    private void findMarco() {
+        HashSet<GameObject> foundObjects = PhotonNetwork.FindGameObjectsWithComponent(typeof(PhotonView));
+        foreach (GameObject obj in foundObjects) {
+            if (obj.name == "Marco(Clone)") {
+                Marco = obj;
+                Debug.Log("Marco found");
+                break;
+            }
+        }
+    }       
 
 }
 
